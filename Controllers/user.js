531a465c08319a7b64b5515948369155c98e37b2
myGithub123# <royandugu99009@gmail.com>
2022-08-10 16:11:42 +0545

    Password hashing

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,10 +1,17 @@
 const {StatusCodes}=require("http-status-codes");
+const bcrypt=require("bcryptjs");
+
 const userModel=require("../Models/user");
 
 const register=async (req,res)=>{
-    const result=await userModel.create({...req.body});
+    const {name,email,password}=req.body;
+    const salt=await bcrypt.genSalt(10);
+    const hashedPassword=await bcrypt.hash(password,salt);
+    const newUser={name,email,password:hashedPassword};
+    const result=await userModel.create({...newUser});
     res.status(StatusCodes.CREATED).json({result:result});
 }
+
 const login=async(req,res)=>{
     console.log("Login User");
 }
