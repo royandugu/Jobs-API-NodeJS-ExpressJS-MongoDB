086867a2b861af7ed9fdfd7064a564d12f5bdab1
myGithub123# <royandugu99009@gmail.com>
2022-08-17 14:45:34 +0545

    Call for instance method

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,14 +1,14 @@
 const {StatusCodes}=require("http-status-codes");
-const bcrypt=require("bcryptjs");
 
 const userModel=require("../Models/user");
 
 const register=async (req,res)=>{
     const result=await userModel.create({...req.body});
-    res.status(StatusCodes.CREATED).json({result:result});
+    const token=result.generateToken();
+    res.status(StatusCodes.CREATED).json({result:result,token:token});
 }
 
 const login=async(req,res)=>{
     console.log("Login User");
 }
-module.exports={register,login}
+module.exports={register,login};
