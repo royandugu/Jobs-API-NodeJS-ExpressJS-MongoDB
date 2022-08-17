30558740e16eab09e374a64ba0ae21c4ee1330c9
myGithub123# <royandugu99009@gmail.com>
2022-08-17 14:46:25 +0545

    Returning error

--- Error_Handlers/errorController.js
+++ Error_Handlers/errorController.js
@@ -3,6 +3,6 @@ const {StatusCodes}=require("http-status-codes");
 const CustomAPIError=require("./customAPIError");
 const errorController=async (err,req,res,next)=>{
     if(err instanceof CustomAPIError) res.status(err.statusCode).json({message:err.message});
-    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"Something went wrong, please try again"});
+    else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:err});
 }
 module.exports=errorController;
