d02a8551bd756a76b2fdeaf8511b9e44bd0bb57f
myGithub123# <royandugu99009@gmail.com>
2022-08-19 11:51:05 +0545

    Fixing status codes

--- Error_Handlers/authenticationError.js
+++ Error_Handlers/authenticationError.js
@@ -1,9 +1,10 @@
+const {StatusCodes}=require("http-status-codes");
 const CustomAPIError=require("./customAPIError");
 
 class AuthenticationError extends CustomAPIError{
     constructor(message){
         super(message);
-        this.statusCode=404;
+        this.statusCode=StatusCodes.UNAUTHORIZED;
     }
 }
 module.exports=AuthenticationError;
--- Error_Handlers/badRequestError.js
+++ Error_Handlers/badRequestError.js
@@ -1,9 +1,10 @@
+const {StatusCodes}=require("http-status-codes");
 const CustomAPIError = require("./customAPIError");
 
 class BadRequestError extends CustomAPIError{
     constructor(message){
         super(message);
-        this.statusCode=401;
+        this.statusCode=StatusCodes.BAD_REQUEST;
     }
 }
 module.exports=BadRequestError;
