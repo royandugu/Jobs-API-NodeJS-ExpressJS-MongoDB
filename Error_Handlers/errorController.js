5146a693b904f47daf7196507b8805a59a67c640
myGithub123# <royandugu99009@gmail.com>
2022-09-28 08:29:23 +0545

    Cast/Validation error handling

--- Error_Handlers/errorController.js
+++ Error_Handlers/errorController.js
@@ -1,15 +1,22 @@
 const {StatusCodes}=require("http-status-codes");
 
-const CustomAPIError=require("./customAPIError");
 const errorController=async (err,req,res,next)=>{
     let errorObject={
         statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg:err.message || "Internal Server error"
     }
-    if(err instanceof CustomAPIError) return res.status(err.statusCode).json({message:err.message});
-    else if(err.code && err.code===11000) {
+    if(err.name==="CastError"){
+        errorObject.statusCode=StatusCodes.BAD_REQUEST
+        errorObject.msg=`The job of id : ${err.value} doesnot exist`
+    }
+    if(err.name==="ValidationError"){
+        console.log(Object.values(err.errors))
         errorObject.statusCode=StatusCodes.BAD_REQUEST;
-        errorObject.msg="The email you are trying to register with is not avaliable";
+        errorObject.message=Object.values(err.errors).map(item=>item.message).join(',');
+    }
+    if(err.code && err.code===11000) {
+        errorObject.statusCode=StatusCodes.BAD_REQUEST;
+        errorObject.msg=`Duplicate value entered for ${Object.keys(err.keyValue)}`;
     }
     res.status(errorObject.statusCode).json({message:errorObject.msg});
 }
